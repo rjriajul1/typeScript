2b6f8cf664a76a6cb13b7f3746ba2b8d76394d31
Riajul Islam <rjriajul1>
2025-11-09 10:25:53 +0600

    non primitive with typeScript

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,20 @@
+//* array, object
+
+let bazarList : string[]= ['eggs', 'milk', 'sugar']
+
+// bazarList.push(true)
+
+let mixedArray : (string | number)[] = ['eggs', 4 , "milk", 1, "sugar", 2]
+
+mixedArray.push(3)
+mixedArray.push('apple')
+// mixedArray.push(true)
+let coordinates : [number,number] = [20,30,]
+let coupe : [string, string] =  ['Husband', 'wife']
+
+let RiajulNameAndRoll : [string,number] = ['Riajul', 30]
+
+// RiajulNameAndRoll[0] = 20
+
+let destination : [string,string,number] = 
+['Dhaka', 'Satkhira', 4]
