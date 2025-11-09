c39e6c347247fc9d0fd7c61e86517fb03a7ea20a
Riajul Islam <rjriajulislam9@gmail.com>
2025-11-10 02:05:46 +0600

    non primitive

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -1,20 +1,56 @@
 //* array, object
 
-let bazarList : string[]= ['eggs', 'milk', 'sugar']
+let bazarList: string[] = ["eggs", "milk", "sugar"];
 
 // bazarList.push(true)
 
-let mixedArray : (string | number)[] = ['eggs', 4 , "milk", 1, "sugar", 2]
+let mixedArray: (string | number)[] = ["eggs", 4, "milk", 1, "sugar", 2];
 
-mixedArray.push(3)
-mixedArray.push('apple')
+mixedArray.push(3);
+mixedArray.push("apple");
 // mixedArray.push(true)
-let coordinates : [number,number] = [20,30,]
-let coupe : [string, string] =  ['Husband', 'wife']
+let coordinates: [number, number] = [20, 30];
+let coupe: [string, string] = ["Husband", "wife"];
 
-let RiajulNameAndRoll : [string,number] = ['Riajul', 30]
+let RiajulNameAndRoll: [string, number] = ["Riajul", 30];
 
 // RiajulNameAndRoll[0] = 20
 
-let destination : [string,string,number] = 
-['Dhaka', 'Satkhira', 4]
+let destination: [string, string, number] = ["Dhaka", "Satkhira", 4];
+
+// reference = ojbect
+
+// const user: {
+//   orginzation: "Programming Hero"; //value => type literal types
+//   isMarried: Boolean;
+//   firstName: String;
+//   middleName?: String;
+//   lastName: String;
+// } = {
+//   firstName: "Riajul",
+//   // middleName: 'Karim',
+//   lastName: "Islam",
+//   isMarried: false,
+//   orginzation: "Programming Hero",
+// };
+// // user.orginzation = "Programming Hero Fire"
+
+// console.log(user);
+
+const user: {
+  readonly orginzation: String; // access modifire
+  isMarried: Boolean;
+  firstName: String;
+  middleName?: String;
+  lastName: String;
+} = {
+  firstName: "Riajul",
+  // middleName: 'Karim',
+  lastName: "Islam",
+  isMarried: false,
+  orginzation: "Programming Hero",
+};
+
+// user.orginzation = "Programming Hero Fire"
+
+console.log(user);
