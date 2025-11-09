39835f020f131de328e865a540c1dd8895d76d3e
Riajul Islam <rjriajul1>
2025-11-09 10:26:43 +0600

    non primitive

---
+++ dist/nonPrimitive.js
@@ -0,0 +1,15 @@
+"use strict";
+//* array, object
+Object.defineProperty(exports, "__esModule", { value: true });
+let bazarList = ['eggs', 'milk', 'sugar'];
+// bazarList.push(true)
+let mixedArray = ['eggs', 4, "milk", 1, "sugar", 2];
+mixedArray.push(3);
+mixedArray.push('apple');
+// mixedArray.push(true)
+let coordinates = [20, 30,];
+let coupe = ['Husband', 'wife'];
+let RiajulNameAndRoll = ['Riajul', 30];
+// RiajulNameAndRoll[0] = 20
+let destination = ['Dhaka', 'Satkhira', 4];
+//# sourceMappingURL=nonPrimitive.js.map
