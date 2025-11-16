7f1b8f9b250c465f5346c514378b3ab757e92af8
Riajul Islam <rjriajul1>
2025-11-16 18:33:24 +0600

    inheritance

--- src/inheritance.ts
+++ src/inheritance.ts
@@ -1,10 +1,11 @@
 //* inheritance
 
 
-class Student {
-    name: string;
-    age: number;
-    address: string
+class Person
+ {
+    name: string; // common
+    age: number; // common
+    address: string // common 
 
     constructor (name: string, age: number,
          address: string
@@ -14,10 +15,46 @@ class Student {
         this.address = address
     }
 
-    getSleep () {
-        console.log(`Eni eto ghonta ghumi`);
+    // common method
+    getSleep (numOfHours: number) {
+        console.log(`${this.name} eto ghonta ${numOfHours}`);
     }
 }
 
-const student1 = new Student('Riajul', 21, 'dhaka')
-console.log(student1);
+class Student extends Person
+{
+
+    rollNo: number;
+
+    constructor(name:string, age:number, address: string, rollNo: number) {
+        super(name,age, address)
+        this.rollNo = rollNo
+    }
+    
+}
+
+const student1 = new Student('Riajul', 21, 'dhaka',10)
+student1.getSleep(15)
+// console.log(student1); 
+   
+
+
+class Teacher extends Person
+ {
+
+    designation: string  // extra property
+
+    constructor ( name: string, age: number, address: string, designation: string
+    ){
+        super(name,age,address)
+        this.designation = designation;
+    }
+
+    // own method
+    takeClass (numOfHours: number) {
+        console.log(`${this.name} eato ghonta class nai ${numOfHours} `);
+    }
+}
+
+const teacher1 = new Teacher('Hossain', 30, 'dhaka', 'senior teacher')
+
